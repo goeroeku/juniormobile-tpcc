5d9729d58278579367e19361ed37ac2254051c4a
goeroeku <goeroeku@gmail.com>
2020-05-13 08:35:24 +0700

    Day 1: - create new project - create form activity and layout - add floating action button to main layout

---
+++ ContactApp/app/src/main/java/com/aiczone/contactapp/FormActivity.java
@@ -0,0 +1,70 @@
+package com.aiczone.contactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class FormActivity extends AppCompatActivity {
+
+    private EditText etName, etDateOfBirth, etEmail, etPhone;
+    private Spinner spProfession;
+    private Button bnSave;
+
+    // data
+    String dateOfBirth = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        etDateOfBirth = findViewById(R.id.etDateOfBirth);
+        etDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                final int mYear = c.get(Calendar.YEAR);
+                final int mMonth = c.get(Calendar.MONTH);
+                final int mDay = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(FormActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                etDateOfBirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                dateOfBirth = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        spProfession = findViewById(R.id.spProfession);
+        String[] profession = {"Mahasiswa", "Dosen", "Karyawan", "PNS"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, profession);
+        spProfession.setAdapter(adapter);
+
+        bnSave = findViewById(R.id.bnSave);
+
+        bnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("bnSave", "bnSave Clicked!");
+            }
+        });
+    }
+}
